1d30c3b46b4fe78afde8d5057a2d9d9ff4967ec2
Keerthan A K <157509004+ManKind07@users.noreply.github.com>
2025-10-25 14:22:20 +0530

    Update Transcription.py

--- Transcription.py
+++ Transcription.py
@@ -1,23 +1,24 @@
 #Transcription from Audio with proper pauses
+#Running on google colab coz its easier to setup and less hassle to run
 
 import json
 from faster_whisper import WhisperModel
 
-# Use tiny or small for less memory usage
+# Using tiny model coz its enough for our use case as well as memory efficient
 model_size = "tiny.en"
-device = "cpu"  # safer on Colab Free
+device = "cpu"  #GPU can be used, but again CPU is enough for our use case
 model = WhisperModel(model_size, device=device)
 
-audio_file = "real_audio.mp3"
+audio_file = "[Insert your audio File]"
 
-# Transcribe audio with word-level timestamps
+#Transcribe of sample audio with word-level timestamps
 segments, info = model.transcribe(audio_file, beam_size=5, word_timestamps=True)
 
 word_list = []
 
 for segment in segments:
     if not hasattr(segment, 'words'):
-        continue  # skip if no word-level info
+        continue  #skips if no word-level info
 
     for word_info in segment.words:
         word_list.append({
@@ -26,10 +27,11 @@ for segment in segments:
             "end": round(word_info.end, 2)
         })
 
-# Save to JSON
+#Save as JSON
 with open("word_timestamps_oneperline.json", "w", encoding="utf-8") as f:
     json.dump(word_list, f, indent=2)
 
 print("Transcription saved. Sample:")
 print(word_list[:])
+
 print("#Process Completed")
